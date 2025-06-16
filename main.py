fe354deccc488698853d245d779a3d1817081b21
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2025-06-16 12:53:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ sites = [
   "pingsite1.streamlit.app"
 ]
 
+def cmd(command):
+  os.system(command)
+
+cmd(apt-get update -y)
+cmd(apt-get install -y iputils-ping)
+
 st.title("Streamlit Ping Site 2")
 st.subheader("Pinging Sites:")
 
@@ -15,6 +21,6 @@ for site in sites:
 while True:
   
   for site in sites:
-    os.system(f"ping {site}")
+    cmd(f"ping {site}")
 
   time.sleep(3600)
